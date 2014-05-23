fb65dbaebab3210250f01545bccf3e195594b10f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-05-23 00:32:28 -0400

    Database interface

---
+++ src/db/Database.js
@@ -0,0 +1,16 @@
+/*#ifndef(UMD)*/
+(function () { /* Begin of privacy scope */
+    "use strict";
+/*#endif*/
+
+    gpf.define("pkml.db.Database", "gpf.interfaces.Interface", {
+
+        public: {
+
+        }
+
+    });
+
+/*#ifndef(UMD)*/
+}()); /* End of privacy scope */
+/*#endif*/
