08f9cf15772a82b30cea4e2862aca42714070387
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-05-29 13:10:03 -0400

    PokeMul core

---
+++ src/core.js
@@ -0,0 +1,16 @@
+/*#ifndef(UMD)*/
+(function () { /* Begin of privacy scope */
+    "use strict";
+/*#endif*/
+
+    /*
+     * Create the initial namespace
+     */
+    if ("undefined" === typeof pkml) {
+        gpf.context().pkml = {};
+    }
+
+
+
+
+}()); /* End of privacy scope */
